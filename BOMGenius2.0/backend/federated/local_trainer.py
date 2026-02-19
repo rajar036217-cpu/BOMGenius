2e5fd62abff01c38451f35dd49e9c1ff05fd3951
Keerthana P G <keerthana.pg2020@gmail.com>
2026-02-20 02:14:09 +0530

    FL completed

--- BOMGenius2.0/backend/federated/local_trainer.py
+++ BOMGenius2.0/backend/federated/local_trainer.py
@@ -1,29 +1,26 @@
 import json
 import os
 
-LOCAL_UPDATES_FILE = "federated/local_updates.json"
-
+LOCAL_RULES_FILE = "federated/global_rules.json"
 
 def log_human_feedback(feedback: dict):
-    """
-    feedback example:
-    {
-      "part_no": "CPU-778",
-      "correct_make_buy": "BUY",
-      "correct_uom": "EA",
-      "correct_work_center": "Assembly"
-    }
-    """
-    updates = []
-    if os.path.exists(LOCAL_UPDATES_FILE):
-        updates = json.load(open(LOCAL_UPDATES_FILE))
 
-    updates.append(feedback)
-    with open(LOCAL_UPDATES_FILE, "w") as f:
-        json.dump(updates, f, indent=2)
+    ebom_part = feedback["ebom_part"]
+    correct_part = feedback["correct_part"]
+
+    rules = {}
+
+    if os.path.exists(LOCAL_RULES_FILE):
+        rules = json.load(open(LOCAL_RULES_FILE))
+
+    rules[ebom_part] = correct_part
+
+    with open(LOCAL_RULES_FILE, "w") as f:
+        json.dump(rules, f, indent=2)
+
 
 
 def export_local_updates():
-    if not os.path.exists(LOCAL_UPDATES_FILE):
+    if not os.path.exists(LOCAL_RULES_FILE):
         return []
-    return json.load(open(LOCAL_UPDATES_FILE))
+    return json.load(open(LOCAL_RULES_FILE))
